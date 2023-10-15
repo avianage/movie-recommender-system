7a5f758abc32ff518246d9056e6c3c92f2325694
avianage <89604905+avianage@users.noreply.github.com>
2023-10-15 19:06:25 +0530

    Added TMDB API to Display Recommended Movie poster

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import pickle
 import pandas as pd
 from flask import Flask, render_template, request
 import zipfile
+import requests
+from builtins import zip
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNjYwNTAwZTE2NGQ3YWE1MjdmMzgwMzZmMzc2MzQ0OSIsInN1YiI6IjY1MmE5OTRmMzU4ZGE3MDBjNmYwZjk1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bc70f35UUGqZocXyZm-jX_ErYNZ-MxUWUY5HhuLDF2s"
+}
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{movie_id}?language=en-US".format(movie_id = movie_id)
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    poster_path = data["poster_path"]
+    # return data['poster_path']
+    return "https://image.tmdb.org/t/p/original"+ poster_path
+
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -9,11 +25,20 @@ def recommend(movie):
     movie_list = sorted(list(enumerate(distances)), reverse=True, key = lambda x:x[1])[1:7]
     
     recommend_movies = []
+    recommend_movie_posters = []
     
     for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        
         recommend_movies.append(movies.iloc[i[0]].title)
+
+        # Fetch from API
+        recommend_movie_posters.append(fetch_poster(movie_id))
+        
+    return recommend_movies, recommend_movie_posters
+
     
-    return recommend_movies     
+
 
 movie_dict = {}
 similarity = []
@@ -28,7 +53,7 @@ with zipfile.ZipFile("pickle_files/pickle_files.zip")  as zf:
             similarity = pickle.load(open('pickle_files/similarity.pkl', 'rb'))
             
 movies = pd.DataFrame(movie_dict)
-        
+
 # movie_dict = pickle.load(open('pickle_files/movies.pkl','rb'))
 # movies = pd.DataFrame(movie_dict)
 
@@ -39,6 +64,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     recommended_movie = []
+    movie_poster = []
     
     movie = movies['title'].values
     
@@ -47,9 +73,9 @@ def index():
     else:
         selected_movie = movie[0]
         
-    recommended_movie = recommend(selected_movie)
+    recommended_movie, movie_poster = recommend(selected_movie)
     
-    return render_template('index.html', selected_movie = selected_movie, movies = movie, recommendations = recommended_movie)
+    return render_template('index.html', selected_movie = selected_movie, movies = movie, recommendations = recommended_movie, movie_poster = movie_poster, zip = zip)
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
